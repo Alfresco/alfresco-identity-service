6eb36cf7eae6764135efbdc8ee43f45b1135a856
Jared Ottley <jared.ottley@alfresco.com>
2018-08-30 23:26:12 -0600

    [AUTH-180] Develop SAML authentication flow for tests / [AUTH-112] Develop SAML automation tests
    -Update pom for dependencies
    -Refactor to no longer be a springboot app
    -Add the ability to set properties in either a properties file or as environment variables
    -Add JWT verification

---
+++ test/saml/src/test/java/org/alfresco/identity/service/saml/test/TokenTestConstants.java
@@ -0,0 +1,34 @@
+package org.alfresco.identity.service.saml.test;
+
+/**
+ * @author Jared Ottley
+ */
+
+public final class TokenTestConstants
+{
+    //Envionment Variables
+    public final static String ENV_SAML_USERNAME = "SAML_USERNAME";
+    public final static String ENV_SAML_PASSWORD = "SAML_PASSWORD";
+    public final static String ENV_KEYCLOAK_HOSTNAME = "KEYCLOAK_HOSTNAME";
+    public final static String ENV_KEYCLOAK_ISSUER= "KEYCLOAK_ISSUER";
+    public final static String ENV_KEYCLOAK_PUBLICKEY = "KEYCLOAK_PUBLICKEY";
+
+    //Property File Keys
+    public final static String PROP_SAML_USERNAME = "saml.username";
+    public final static String PROP_SAML_PASSWORD = "saml.password";
+    public final static String PROP_KEYCLOAK_HOSTNAME = "keycloak.hostname";
+    public final static String PROP_KEYCLOAK_ISSUER= "keycloak.issuer";
+    public final static String PROP_KEYCLOAK_PUBLICKEY = "keycloak.publickey";
+    
+    //Page Elements
+    public final static String ELEMENT_SAML = "saml";
+    public final static String ELEMENT_USERID = "userid";
+    public final static String ELEMENT_PASWORD = "password";
+
+    //Header Parameters
+    public final static String HEADER_ACCESS_TOKEN = "access_token";
+
+    //Key Factory Algorithims
+    public final static String ALGORITHIM_RSA = "RSA";
+
+}
