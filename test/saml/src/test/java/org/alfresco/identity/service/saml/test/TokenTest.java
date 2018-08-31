6464a1c3f1f74f5b445947d00a5e557f1d64a061
Jared Ottley <jared.ottley@alfresco.com>
2018-08-30 23:26:12 -0600

    [AUTH-180] Develop SAML authentication flow for tests / [AUTH-112] Develop SAML automation tests
    -Update pom for dependencies
    -Refactor to no longer be a springboot app
    -Add the ability to set properties in either a properties file or as environment variables
    -Add JWT verification

---
+++ test/saml/src/test/java/org/alfresco/identity/service/saml/test/TokenTest.java
@@ -0,0 +1,220 @@
+package org.alfresco.identity.service.saml.test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import org.openqa.selenium.By.ByLinkText;
+import org.openqa.selenium.By.ByName;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @author Jared Ottley
+ */
+@TestInstance(Lifecycle.PER_CLASS)
+ public class TokenTest
+{  
+    private Logger logger = LoggerFactory.getLogger(TokenTest.class);
+    private Properties appProps = null;
+
+
+    @BeforeAll
+    void setup()
+    {
+        try
+        {
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String appConfigPath = rootPath + "application.properties";
+ 
+        appProps = new Properties();
+        appProps.load(new FileInputStream(appConfigPath));
+        }
+        catch (IOException exception)
+        {
+            logger.info("Unable to read properties file");
+        }
+    }
+
+    @Test
+    void getTokenFromSAMLLogin() 
+    throws MalformedURLException, 
+           IOException, 
+           NoSuchAlgorithmException, 
+           InvalidKeySpecException, 
+           Exception
+    {
+        //Create HTMLUnit WebDriver
+        WebDriver driver = new HtmlUnitDriver(true);
+        
+        //Initiate page
+        driver.get("https://" + getHostname() + "/auth/realms/alfresco/protocol/openid-connect/auth?response_type=id_token%20token&client_id=alfresco&state=CIteJYtFrA22JnCikKHJ2QPrNuGHzyOphE1SsSNs&redirect_uri=http%3A%2F%2Flocalhost%2Fdummy_redirect&scope=openid%20profile%20email&nonce=CIteJYtFrA22JnCikKHJ2QPrNuGHzyOphE1SsSNs");
+       
+        //Click on SAML link on login page
+        WebElement element = driver.findElement(ByLinkText.linkText(TokenTestConstants.ELEMENT_SAML));
+        element.click();
+
+        //Select User, Enter password, and submit form on SAML page
+        Select select = new Select(driver.findElement(ByName.name(TokenTestConstants.ELEMENT_USERID)));
+        select.selectByVisibleText(getUser());
+        WebElement passwordfield = driver.findElement(ByName.name(TokenTestConstants.ELEMENT_PASWORD));
+        passwordfield.sendKeys(getPassword());
+        passwordfield.submit();
+
+        //Get the redirect URL for validation -- If you check the status of the
+        //redirct URL call it will be 404.  The page does not exist. All we are
+        //intersted in is the token parameter in the URL
+        logger.info("Redirect URL: " + driver.getCurrentUrl());
+
+        //Get token param
+        Map<String, String> params = getQueryStringMap(driver.getCurrentUrl());
+        String token = params.get(TokenTestConstants.HEADER_ACCESS_TOKEN);
+        logger.info("access_token parameter: " + token);
+        
+
+        //Decode token and verify token
+        DecodedJWT jwt = null;
+        try
+        {
+            KeyFactory kf = KeyFactory.getInstance(TokenTestConstants.ALGORITHIM_RSA);
+
+            X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(Base64.decodeBase64(getPublickey()));
+            RSAPublicKey pubKey = (RSAPublicKey) kf.generatePublic(keySpecX509);
+            Algorithm algorithm = Algorithm.RSA256(pubKey, null);
+            JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer(getIssuer())
+                .build(); //Reusable verifier instance
+            jwt = verifier.verify(token);
+            logger.info("Payload Decoded: " + new String(Base64.decodeBase64(jwt.getPayload().getBytes())));
+        }
+        catch (JWTVerificationException exception)
+        {
+            logger.info("Verfication failed");
+        }
+
+        assertNotNull(jwt);
+        
+        //Quit Driver session
+        driver.quit();
+
+    }
+
+    //Utility Methods
+    private Map<String, String> getQueryStringMap(String url)
+    {
+        Map<String, String> map = new HashMap<>();
+
+        if (StringUtils.isNotBlank(url))
+        {
+            String[] split = url.split("#");
+
+            if(split != null && split.length == 2 && StringUtils.isNotBlank(split[1]))
+            {
+                String[] params = split[1].split("&");
+
+                if (params != null && params.length > 0)
+                {
+                    for (String param : params)
+                    {
+                        String[] working = param.split("=");
+
+                        if (working != null && working.length >= 1 && StringUtils.isNotBlank(working[0]))
+                        {
+                            map.put(working[0], ((working.length == 2 && StringUtils.isNotBlank(working[1])) ? working[1] : ""));
+                        }
+                    }
+                }
+            }
+        }
+
+        return map;
+    }
+
+    private String getHostname()
+    {
+        String keycloak_hostname = System.getenv(TokenTestConstants.ENV_KEYCLOAK_HOSTNAME);
+        
+        if (StringUtils.isEmpty(keycloak_hostname))
+        {
+            keycloak_hostname = appProps.getProperty(TokenTestConstants.PROP_KEYCLOAK_HOSTNAME);
+        }
+
+        return keycloak_hostname;
+    }
+
+    private String getIssuer()
+    {
+        String keycloak_issuer = System.getenv(TokenTestConstants.ENV_KEYCLOAK_ISSUER);
+        
+        if (StringUtils.isEmpty(keycloak_issuer))
+        {
+            keycloak_issuer = appProps.getProperty(TokenTestConstants.PROP_KEYCLOAK_ISSUER);
+        }
+
+        return keycloak_issuer;
+    }
+
+    private String getPublickey()
+    {
+        String keycloak_publickey = System.getenv(TokenTestConstants.ENV_KEYCLOAK_PUBLICKEY);
+        
+        if (StringUtils.isEmpty(keycloak_publickey))
+        {
+            keycloak_publickey = appProps.getProperty(TokenTestConstants.PROP_KEYCLOAK_PUBLICKEY);
+        }
+
+        return keycloak_publickey;
+    }
+
+    private String getUser()
+    {
+        String saml_user = System.getenv(TokenTestConstants.ENV_SAML_USERNAME);
+        
+        if (StringUtils.isEmpty(saml_user))
+        {
+            saml_user = appProps.getProperty(TokenTestConstants.PROP_SAML_USERNAME);
+        }
+
+        return saml_user;
+    }
+
+    private String getPassword()
+    {
+        String saml_password = System.getenv(TokenTestConstants.ENV_SAML_PASSWORD);
+        
+        if (StringUtils.isEmpty(saml_password))
+        {
+            saml_password = appProps.getProperty(TokenTestConstants.PROP_SAML_PASSWORD);
+        }
+
+        return saml_password;
+    }
+    
+}
