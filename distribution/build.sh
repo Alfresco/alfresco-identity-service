86c0271aa518a4ca106b536de45e47754b2861a6
svidrascu <sergiu.vidrascu@ness.com>
2019-01-04 12:51:09 +0200

    AUTH-200: Added distribution folder and build files

---
+++ distribution/build.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+set -o errexit
+
+source ./build.properties
+
+mkdir alfresco-identity-$IDENTITY_VERSION
+
+echo "Downloading keycloak"
+curl -O https://downloads.jboss.org/keycloak/$KEYCLOAK_VERSION/keycloak-$KEYCLOAK_VERSION.zip
+curl -O https://downloads.jboss.org/keycloak/$KEYCLOAK_VERSION/keycloak-$KEYCLOAK_VERSION.zip.sha1
+sha1sum -c keycloak-$KEYCLOAK_VERSION.zip.sha1
+
+echo "unzipping keycloak"
+unzip keycloak-$KEYCLOAK_VERSION.zip
+
+echo "adding realm"
+mkdir -p keycloak-$KEYCLOAK_VERSION/realm
+cp chart/helm/alfresco-identity-service/alfresco-realm.json keycloak-$KEYCLOAK_VERSION/realm/
+sed -i'.bak' "
+  s#ALFRESCO_CLIENT_REDIRECT_URIS#[\"*\"]#g;
+" keycloak-$KEYCLOAK_VERSION/realm/alfresco-realm.json
+
+echo "adding themes"
+docker run --rm -v "$PWD/alfresco:/tmp" alfresco/alfresco-keycloak-theme:$THEME_VERSION sh -c "cp -rf /alfresco/* /tmp/"
+cp -rf alfresco keycloak-$KEYCLOAK_VERSION/themes/
+rm -rf alfresco
+ls keycloak-$KEYCLOAK_VERSION/themes/alfresco
+
+echo '# Alfresco realm import ' >> keycloak-$KEYCLOAK_VERSION/bin/standalone.conf
+echo 'JAVA_OPTS="$JAVA_OPTS -Dkeycloak.import=../realm/alfresco-realm.json"' >> keycloak-$KEYCLOAK_VERSION/bin/standalone.conf
+
+echo '\nrem # Alfresco realm import ' >> keycloak-$KEYCLOAK_VERSION/bin/standalone.conf.bat
+echo "set \"JAVA_OPTS=%JAVA_OPTS% -Dkeycloak.import=%~dp0..\\\realm\\\alfresco-realm.json\"\n:JAVA_OPTS_SET" >> keycloak-$KEYCLOAK_VERSION/bin/standalone.conf.bat
+
+echo "\n# Alfresco realm import " >> keycloak-$KEYCLOAK_VERSION/bin/standalone.conf.ps1
+echo "\$JAVA_OPTS += \"-Dkeycloak.import=\$pwd\\\..\\\realm\\\alfresco-realm.json\"" >> keycloak-$KEYCLOAK_VERSION/bin/standalone.conf.ps1
+
+rm -rf alfresco-identity-$IDENTITY_VERSION
+mkdir alfresco-identity-$IDENTITY_VERSION
+cp -rf keycloak-$KEYCLOAK_VERSION/* alfresco-identity-$IDENTITY_VERSION/
+rm -rf keycloak-$KEYCLOAK_VERSION
+ls alfresco-identity-$IDENTITY_VERSION
+
+echo packaging identity
+zip -r alfresco-identity-$IDENTITY_VERSION.zip alfresco-identity-$IDENTITY_VERSION
+sha1sum alfresco-identity-$IDENTITY_VERSION.zip > alfresco-identity-$IDENTITY_VERSION.zip.sha1
