3b239a6932ab91fe9e71725845cdb69760548c52
svidrascu <sergiu.vidrascu@ness.com>
2019-01-11 15:40:51 +0200

    AUTH-200: fix unzip

--- distribution/tests/unzip.ps1
+++ distribution/tests/unzip.ps1
@@ -1,16 +1,11 @@
 ls
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-function unzip {
-    $ziparchive=$args[0]
-    $extractpath=$args[1]
-    [System.IO.Compression.ZipFile]::ExtractToDirectory( $ziparchive, $extractpath )
-}
 
 Get-WmiObject Win32_Process -filter "CommandLine LIKE '%alfresco-identity-service-$IDENTITY_VERSION%'"
 Get-WmiObject Win32_Process -filter "CommandLine LIKE '%alfresco-identity-service-$IDENTITY_VERSION%'" | select-object -Property ProcessId
 Get-WmiObject Win32_Process -filter "CommandLine LIKE '%alfresco-identity-service-$IDENTITY_VERSION%'" | foreach { kill $_.ProcessId }
 
-unzip $WORKING_DIR\alfresco-identity-service-$IDENTITY_VERSION.zip $WORKING_DIR
-
-ls
+$ziparchive = "$WORKING_DIR\alfresco-identity-service-$IDENTITY_VERSION.zip"
+$extractpath = "$WORKING_DIR"
+[System.IO.Compression.ZipFile]::ExtractToDirectory( $ziparchive, $extractpath )
