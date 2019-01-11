16b9fa332a30d3211c56f4a4ac9bb4390b54452f
svidrascu <sergiu.vidrascu@ness.com>
2019-01-11 15:15:38 +0200

    AUTH-200: remove unused echos

--- distribution/tests/unzip.ps1
+++ distribution/tests/unzip.ps1
@@ -2,16 +2,15 @@ ls
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 function unzip {
-    param( [string]$ziparchive, [string]$extractpath )
+    $ziparchive=$args[0]
+    $extractpath=$args[1]
     [System.IO.Compression.ZipFile]::ExtractToDirectory( $ziparchive, $extractpath )
 }
 
-echo $IDENTITY_VERSION
-echo $WORKING_DIR
 Get-WmiObject Win32_Process -filter "CommandLine LIKE '%alfresco-identity-service-$IDENTITY_VERSION%'"
 Get-WmiObject Win32_Process -filter "CommandLine LIKE '%alfresco-identity-service-$IDENTITY_VERSION%'" | select-object -Property ProcessId
 Get-WmiObject Win32_Process -filter "CommandLine LIKE '%alfresco-identity-service-$IDENTITY_VERSION%'" | foreach { kill $_.ProcessId }
 
-unzip "$WORKING_DIR\alfresco-identity-service-$IDENTITY_VERSION.zip" "$WORKING_DIR"
+unzip $WORKING_DIR\alfresco-identity-service-$IDENTITY_VERSION.zip $WORKING_DIR
 
 ls
