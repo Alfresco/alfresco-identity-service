e36f6f990c9d9caf6a9d9644191f7f433552669f
Cristina Sirbu <cristina.sirbu@ness.com>
2019-10-18 15:57:20 +0300

    AUTH-392: Change paths again

--- distribution/build.sh
+++ distribution/build.sh
@@ -72,17 +72,17 @@ echo "\$JAVA_OPTS += \"-Dkeycloak.import=\$pwd\\\..\\\realm\\\alfresco-realm.jso
 mkdir -p $KEYCLOAK_NAME/modules/system/layers/base/com/mysql/jdbc/main
 cd $KEYCLOAK_NAME/modules/system/layers/base/com/mysql/jdbc/main
 curl -O https://repo1.maven.org/maven2/mysql/mysql-connector-java/$JDBC_MYSQL_VERSION/mysql-connector-java-$JDBC_MYSQL_VERSION.jar
-cp /opt/jboss/tools/databases/mysql/module.xml .
+cp docker/databases/mysql/module.xml .
 
 mkdir -p $KEYCLOAK_NAME/modules/system/layers/base/org/postgresql/jdbc/main
 cd $KEYCLOAK_NAME/modules/system/layers/base/org/postgresql/jdbc/main
 curl -L https://repo1.maven.org/maven2/org/postgresql/postgresql/$JDBC_POSTGRES_VERSION/postgresql-$JDBC_POSTGRES_VERSION.jar > postgres-jdbc.jar
-cp /opt/jboss/tools/databases/postgres/module.xml .
+cp docker/databases/postgres/module.xml .
 
 mkdir -p $KEYCLOAK_NAME/modules/system/layers/base/org/mariadb/jdbc/main
 cd $KEYCLOAK_NAME/modules/system/layers/base/org/mariadb/jdbc/main
 curl -L https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/$JDBC_MARIADB_VERSION/mariadb-java-client-$JDBC_MARIADB_VERSION.jar > mariadb-jdbc.jar
-cp /opt/jboss/tools/databases/mariadb/module.xml .
+cp docker/databases/mariadb/module.xml .
 
 ######################
 # Configure Keycloak #
