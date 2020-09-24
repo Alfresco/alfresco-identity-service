06566d1ae5049901a242052a36e3b39f7bbb32ca
Lucian Suciu <lucian.suciu@ness.com>
2020-09-24 10:52:55 +0300

    try again

--- distribution/tests/endpoints_ps.ps1
+++ distribution/tests/endpoints_ps.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 Get-WmiObject Win32_Process -filter "CommandLine LIKE '%alfresco-identity-service-$IDENTITY_VERSION%'"
 
 ./standalone.ps1 --background
