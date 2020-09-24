64953bf54a5eea5f70de9ea0411fc5f41a3ad1de
Lucian Suciu <lucian.suciu@ness.com>
2020-09-24 21:28:57 +0300

    curl params

--- distribution/tests/endpoints_ps.ps1
+++ distribution/tests/endpoints_ps.ps1
@@ -30,9 +30,9 @@ if ($SERVICEUP -ne 1) {
     throw "Identity Service timed out "
 }
 
-checkStatus (curl -v http://localhost:8080/auth/).StatusCode "200"
-checkStatus (curl -v http://localhost:8080/auth/admin/alfresco/console/).StatusCode "200"
-checkStatus (curl -sSL "http://localhost:8080/auth/realms/alfresco/account").Content "Identity"
+checkStatus (curl -UseBasicParsing -v http://localhost:8080/auth/).StatusCode "200"
+checkStatus (curl -UseBasicParsing -v http://localhost:8080/auth/admin/alfresco/console/).StatusCode "200"
+checkStatus (curl -UseBasicParsing -SslProtocol "http://localhost:8080/auth/realms/alfresco/account").Content "Identity"
 $Body = @{
    client_id = "alfresco"
    username = "admin"
