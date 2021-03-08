be64f493e5df1f251c896ce7c0f74a3642b2d7fb
sergiu.vidrascu@ness.com <sergiu.vidrascu@ness.com>
2021-03-08 16:50:51 +0200

    add win and linux fixes

--- distribution/tests/endpoints_ps.ps1
+++ distribution/tests/endpoints_ps.ps1
@@ -32,7 +32,7 @@ if ($SERVICEUP -ne 1) {
 
 checkStatus (curl -UseBasicParsing -v http://localhost:8080/auth/).StatusCode "200"
 checkStatus (curl -UseBasicParsing -v http://localhost:8080/auth/admin/alfresco/console/).StatusCode "200"
-checkStatus (curl -UseBasicParsing -v "http://localhost:8080/auth/realms/alfresco/account").Content "Identity"
+checkStatus (curl -UseBasicParsing -v "http://localhost:8080/auth/realms/alfresco/protocol/openid-connect/auth?client_id=security-admin-console&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Fadmin%2Falfresco%2Fconsole%2F&state=ea46ea9f-c963-4f06-89a9-ea9ec04c9694&response_mode=fragment&response_type=code&scope=openid&nonce=44e4af22-2f82-47ef-864e-364c662ae884&code_challenge=IfHxRz3ftCUq4h-SXrSsfXGhoH5z-NfkUSCVyPiNEIc&code_challenge_method=S256").Content "Alfresco Identity Service"
 $Body = @{
    client_id = "alfresco"
    username = "admin"
